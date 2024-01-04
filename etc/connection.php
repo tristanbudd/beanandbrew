274a88c54b07b9d9e324196ba5f9a2a5431ae04e
tristanbudd <tristan.budd8@gmail.com>
2024-01-04 23:49:10 +0000

    Database Initial Setup

--- etc/connection.php
+++ etc/connection.php
@@ -4,7 +4,7 @@ $error_message = "";
 
 if (!file_exists("../settings.json")) {
     $error_message = "Error locating settings.json, If not setup go to /etc/installer.php";
-    header("Location: error.php?error_message=$error_message&retry_url=index.php");
+    header("Location: error.php?error_message=$error_message");
 } else {
     $settings_data = file_get_contents("../settings.json");
 
@@ -26,9 +26,24 @@ if (!file_exists("../settings.json")) {
     }
 
     if ($error_amount > 0) {
-        header("Location: error.php?error_message=$error_message&retry_url=index.php");
+        header("Location: error.php?error_message=$error_message");
     } else {
-        echo("<p>Success</p>");
+        $conn = new mysqli($settings_json->server_name, $settings_json->username, $settings_json->password);
+
+        if ($conn->connect_errno) {
+            $error_message = "Failed to connect to MySQL: " . $conn->connect_error;
+            header("Location: error.php?error_message=" . urlencode($error_message));
+            exit();
+        }
+
+        $database_name = $settings_json->database_name;
+        $query = "CREATE DATABASE IF NOT EXISTS $database_name";
+
+        if (!$conn->query($query) === TRUE) {
+            $error_message = "Error creating database: " . $conn->error;
+            header("Location: error.php?error_message=$error_message");
+            exit();
+        }
     }
 }
 
