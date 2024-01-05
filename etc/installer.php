1fa62d8f3aaca94e3922a5587060acf3362f609a
Tristan Budd <tristan.budd8@gmail.com>
2024-01-05 09:27:03 +0000

    Added Icons

--- etc/installer.php
+++ etc/installer.php
@@ -1,12 +1,50 @@
 <?php
-if (!file_exists("../settings.json")) {
-    touch("../settings.json");
-
-    $settings_file = fopen("../settings.json", "w");
-
-    $default_data = '{"server_name": "localhost", "username": "root", "password": "", "database_name": "bean_and_brew"}';
-    fwrite($settings_file, $default_data);
-
-    fclose($settings_file);
+if (file_exists("../settings.json")) {
+    $error_message = "Installation already complete, to reset process delete settings.json";
+    header("Location: error.php?error_message=$error_message");
 }
+
+/*
+touch("../settings.json");
+
+$settings_file = fopen("../settings.json", "w");
+
+$default_data = '{"server_name": "localhost", "username": "root", "password": "", "database_name": "bean_and_brew"}';
+fwrite($settings_file, $default_data);
+
+fclose($settings_file);
+*/
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bean & Brew - Installer</title>
+
+    <meta name="robots" content="noindex, nofollow">
+    <meta name="googlebot" content="noindex, nofollow">
+    <meta name="google" content="nositelinkssearchbox">
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Indulge in the rich flavors of coffee at Bean & Brew. Explore our wide range of coffee blends, brewing equipment, and accessories. Discover tools that elevate your coffee experience, making every sip a moment of joy.">
+    <meta name="keywords" content="Bean & Brew, Coffee, Coffee Website, Coffee Blends, Brewing Equipment, Coffee Accessories, Coffee Flavors, Coffee Shop, Coffee Tools, Coffee Experience, Coffee Moments, Morning Delight, Coffee Lover, Coffee Recipes, Coffee Tips, Coffee Tasting, Coffee Brewing Techniques, Coffee Grinder, Coffee Maker, French Press, Espresso Machine, Coffee Beans, Coffee Filters, Coffee Mugs, Coffee Storage, Coffee Gadgets">
+    <meta name="author" content="Tristan Budd">
+    <meta name="copyright" content="Copyright © Bean & Brew (<?php echo(date("Y")) ?>)" />
+
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-config" content="/favicons/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
+    <link rel="manifest" href="/favicons/site.webmanifest">
+    <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#7b7b7b">
+    <link rel="shortcut icon" href="/favicons/favicon.ico">
+
+    <link rel="stylesheet" href="../style.css" type="text/css">
+</head>
+<body>
+</body>
+</html>
