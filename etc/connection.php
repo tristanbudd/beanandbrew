1e6220191c30b3857a0afbbe497c87da50b4ce41
tristanbudd <tristan.budd8@gmail.com>
2024-01-10 13:29:13 +0000

    Epic Navigation Bar

--- etc/connection.php
+++ etc/connection.php
@@ -2,18 +2,18 @@
 $error_amount = 0;
 $error_message = "";
 
-if (!file_exists("../settings.json")) {
+if (!file_exists("settings.json")) {
     $error_message = "Error locating settings.json, If not setup go to /etc/installer.php";
-    header("Location: error.php?error_message=$error_message");
+    header("Location: etc/error.php?error_message=$error_message");
 } else {
-    $settings_data = file_get_contents("../settings.json");
+    $settings_data = file_get_contents("settings.json");
 
     if ($settings_data === false) {
         $error_message = "Error fetching settings.json data. (Not Found)";
         $error_amount += 1;
     }
 
-    if (filesize("../settings.json") == 0 or file_get_contents("../settings.json") == "") {
+    if (filesize("settings.json") == 0 or file_get_contents("settings.json") == "") {
         $error_message = "Error fetching settings.json data. (File Empty)";
         $error_amount += 1;
     }
@@ -26,13 +26,13 @@ if (!file_exists("../settings.json")) {
     }
 
     if ($error_amount > 0) {
-        header("Location: error.php?error_message=$error_message");
+        header("Location: etc/error.php?error_message=$error_message");
     } else {
         $conn = new mysqli($settings_json->server_name, $settings_json->username, $settings_json->password);
 
         if ($conn->connect_errno) {
             $error_message = "Failed to connect to MySQL: " . $conn->connect_error;
-            header("Location: error.php?error_message=" . urlencode($error_message));
+            header("Location: etc/error.php?error_message=" . urlencode($error_message));
             exit();
         }
 
@@ -41,7 +41,7 @@ if (!file_exists("../settings.json")) {
 
         if (!$conn->query($query) === TRUE) {
             $error_message = "Error creating database: " . $conn->error;
-            header("Location: error.php?error_message=$error_message");
+            header("Location: etc/error.php?error_message=$error_message");
             exit();
         }
     }
